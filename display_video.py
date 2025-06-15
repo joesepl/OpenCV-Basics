e841b74f315d47b95a0f32f13e97ddad76e49ef3
Jose Sepulveda <Joseph.h.Sepulveda@gmail.com>
2025-06-14 22:01:40 -0700

    Add display picture, display_video, and display webcam files

---
+++ display_video.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+
+def display_video(window_title, filename):
+    capture = cv.VideoCapture(filename)    # Here you can provide an integer for a camera with the default being 0. Or you can provide a full path to a video file. 
+    print("Video playing press q to quit.")
+    while True:
+        isTrue, frame = capture.read()      # Returns the actual frame of the video, and whether it captured it succesffuly (boolean). Iterates through each frame of the video.
+        if (isTrue):
+            cv.imshow(window_title, frame)
+            if cv.waitKey(20) == ord('q'):      # Waits for 20 ms each loop for a key then returns -1 if no key pressed or the ascii number for pressed key. In this case q will quit the program
+                break
+        else:
+            break
+
+    capture.release()                       # Release camera resource
+    cv.destroyAllWindows()                  # Close opencv windows
+
+
+def main():
+    display_video('Video', 'Photos/dog_vid.mp4')
+
+
+if __name__ == "__main__":
+    main()
